68175c70c8008d0a00c874b9220a3b290600b820
pparksuuu <supr2000@gmail.com>
2019-04-08 16:32:48 +0900

    spring

--- IntelliJ/src/main/java/com/example/demo9/DemoApplication9.java
+++ IntelliJ/src/main/java/com/example/demo9/DemoApplication9.java
@@ -1,15 +1,14 @@
-package com.example.demo7;
+package com.example.demo9;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
-public class DemoApplication {
+public class DemoApplication9 {
 
     public static void main(String[] args) {
-//        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication9.class, args);
 
     }
 }
