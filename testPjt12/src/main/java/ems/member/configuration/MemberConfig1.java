e105b16b1bf48f7cc1837b5e49a0f65c034f7b8c
pparksuuu <supr2000@gmail.com>
2019-04-12 17:58:59 +0900

    study spring

---
+++ testPjt12/src/main/java/ems/member/configuration/MemberConfig1.java
@@ -0,0 +1,48 @@
+package ems.member.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import ems.member.dao.StudentDao;
+import ems.member.service.StudentAllSelectService;
+import ems.member.service.StudentDeleteService;
+import ems.member.service.StudentModifyService;
+import ems.member.service.StudentRegisterService;
+import ems.member.service.StudentSelectService;
+
+@Configuration
+@Import({MemberConfig2.class, MemberConfig3.class})
+public class MemberConfig1 {
+	
+	@Bean
+	public StudentDao studentDao() {// id를 그대로
+		return new StudentDao();
+	}
+	
+	@Bean
+	public StudentRegisterService registerService() {
+		return new StudentRegisterService(studentDao());
+	}
+	
+	@Bean
+	public StudentModifyService modifyService() {
+		return new StudentModifyService(studentDao());
+	}
+	
+	@Bean
+	public StudentDeleteService deleteService() {
+		return new StudentDeleteService(studentDao());
+	}
+	
+	@Bean
+	public StudentSelectService selectService() {
+		return new StudentSelectService(studentDao());
+	}
+	
+	@Bean
+	public StudentAllSelectService allSelectService() {
+		return new StudentAllSelectService(studentDao());
+	}
+		
+}
---
+++ testPjt12/src/main/java/ems/member/configuration/MemberConfig2.java
@@ -0,0 +1,28 @@
+package ems.member.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import ems.member.DataBaseConnectionInfo;
+
+@Configuration
+public class MemberConfig2 {
+	
+	@Bean
+	public DataBaseConnectionInfo dataBaseConnectionInfoDev() {
+		DataBaseConnectionInfo infoDev = new DataBaseConnectionInfo();
+		infoDev.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		infoDev.setUserId("scott");
+		infoDev.setUserPw("tiger");
+		return infoDev;
+	}
+	
+	@Bean
+	public DataBaseConnectionInfo dataBaseConnectionInfoReal() {
+		DataBaseConnectionInfo infoDev = new DataBaseConnectionInfo();
+		infoDev.setJdbcUrl("jdbc:oracle:thin:@192.168.0.1:1521:xe");
+		infoDev.setUserId("masterid");
+		infoDev.setUserPw("masterpw");
+		return infoDev;
+	}
+}
